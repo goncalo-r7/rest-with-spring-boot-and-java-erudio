eae18fc46e0550f579d1f9106b83fc88ab000c0b
goncalo-r7 <161698009+goncalo-r7@users.noreply.github.com>
2026-01-04 23:29:49 +0000

    First Steps with Spring Boot 3.4.0

---
+++ 04_FirstStepsInJavaWithSpringBoot/src/main/java/pt/goncalorodrigues/model/Greeting.java
@@ -0,0 +1,4 @@
+package pt.goncalorodrigues.model;
+
+public record Greeting(long id, String content) {
+}
---
+++ 04_FirstStepsInJavaWithSpringBoot/src/test/java/pt/goncalorodrigues/StartupTests.java
@@ -0,0 +1,13 @@
+package pt.goncalorodrigues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
